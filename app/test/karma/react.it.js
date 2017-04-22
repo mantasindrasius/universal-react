8a4e3c4e7187680ad232c7aad5413697b93cdff3
Mantas Indrašius <mantasi@wix.com>
2017-04-22 22:50:34 +0300

    Karma & jest tests

---
+++ app/test/karma/react.it.js
@@ -0,0 +1,5 @@
+describe('React', function() {
+  it('should render', () => {
+    expect(2).to.equal(2);    
+  });
+})
---
+++ app/test/unit/slides.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Slides', function() {
+  it('should pass', () => {
+    expect(2).is.equal(2);
+  });
+});
