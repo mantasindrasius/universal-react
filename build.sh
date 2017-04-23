c0a41357f52110f07fdfbf80e7684c9a3570938e
Mantas Indrašius <mantasi@wix.com>
2017-04-24 00:51:37 +0300

    Extracted to a compile script

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@
 for build in app server/node server/java
 do
   echo 'Building '$build
-  (cd $build; yarn install; yarn setup; yarn build; yarn test)
+  (cd $build; yarn install; yarn setup; yarn test)
 done
-
-#find . -name package.json -not -path "**/node_modules/**" -exec echo 'Building {}' \; -execdir yarn install \; -execdir yarn test \;
--- server/java/src/NashornReactITSpec.scala
+++ server/java/src/NashornReactITSpec.scala
@@ -1,11 +1,15 @@
 package lt.indrasius.react
 
 import org.specs2.mutable.Specification
+import javax.script.ScriptEngineManager
 
 class HelloSpec extends Specification {
   "Hello" should {
     "should pass" in {
+      val scriptEngineManager = new ScriptEngineManager()
+      val nashorn = scriptEngineManager.getEngineByName("nashorn")
+
       1 must_== 1
     }
-  }
+  }  
 }
