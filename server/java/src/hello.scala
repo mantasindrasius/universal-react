9be9eea0480a68a713353f70dae2fd6b1c93192a
Mantas Indrašius <mantasi@wix.com>
2017-04-23 17:23:28 +0300

    Compile & run scala from NPM

---
+++ server/java/src/hello.scala
@@ -0,0 +1,11 @@
+package lt.indrasius.react
+
+import org.specs2.mutable.Specification
+
+class HelloSpec extends Specification {
+  "Hello" should {
+    "should pass" in {
+      1 must_== 1
+    }
+  }
+}
