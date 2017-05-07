50cb8c7e34b3fba47bfe904e474b228721e1a9bf
Mantas Indrašius <mantasi@wix.com>
2017-05-07 21:38:24 +0300

    Builder app accepting args

--- server/scala-builder/src/builder.scala
+++ server/scala-builder/src/builder.scala
@@ -12,6 +12,7 @@ import scala.reflect.io.VirtualFile
 import java.nio.file.{Files, Paths}
 import java.lang.Package
 import java.net.{URLClassLoader, URL}
+import scopt.OptionDef
 
 class ScalaBuilderSpec extends Spec {
   lazy val builder = new ScalaBuilder(
@@ -26,6 +27,67 @@ class ScalaBuilderSpec extends Spec {
     }
   }
 }
+
+class ScalaBuilderOptionsSpec extends Spec {
+  "ScalaBuilderOptions" should {
+    "parse options with single source" in {
+      ScalaBuilderOptions.parse(Array("src/**")) must_==
+        ScalaBuilderOptions(
+          sources = Seq("src/**"),
+          deps = Nil)
+    }
+
+    "parse options with multiple sources" in {
+      ScalaBuilderOptions.parse(Array("src/**", "a")) must_==
+        ScalaBuilderOptions(
+          sources = Seq("src/**", "a"),
+          deps = Nil)
+    }
+
+    "parse options with single dependency" in {
+      ScalaBuilderOptions.parse(Array("-d", "dep1", "a")) must_==
+        ScalaBuilderOptions(
+          sources = Seq("a"),
+          deps = Seq("dep1"))
+    }
+
+    "parse options with multiple dependencies" in {
+      ScalaBuilderOptions.parse(Array("-d", "dep1", "-d", "dep2", "a")) must_==
+        ScalaBuilderOptions(
+          sources = Seq("a"),
+          deps = Seq("dep1", "dep2"))
+    }
+  }
+}
+
+object ScalaBuilder {
+  def main(args: Array[String]): Unit = {
+    val options = ScalaBuilderOptions.parse(args)
+
+    new ScalaBuilder(options.deps:_*)
+      .build(options.sources.head)
+  }
+}
+
+object ScalaBuilderOptions {
+  val parser = new scopt.OptionParser[ScalaBuilderOptions]("scala-build") {
+    head("scala-build", "1.0")
+
+    arg[Seq[String]]("<sources>").unbounded.action { (sources, c) =>
+      c.copy(sources = c.sources ++ sources)
+    }
+
+    opt[Seq[String]]('d', "dependency").unbounded.action { (deps, c) =>
+      c.copy(deps = c.deps ++ deps)
+    }
+  }
+
+  def parse(args: Array[String]): ScalaBuilderOptions =
+    parser.parse(args, ScalaBuilderOptions()).get
+}
+
+case class ScalaBuilderOptions(sources: Seq[String] = Nil,
+                               deps: Seq[String] = Nil)
 
 class ScalaBuilder(deps: String*) {
   private lazy val classpath: String = {
