05205c6560a8a7e4535fbc0a1d09b93e3839dc93
Mantas Indrašius <mantasi@wix.com>
2017-05-15 21:29:32 +0300

    React -> react

--- app/test/learning/react-noop.spec.js
+++ app/test/learning/react-noop.spec.js
@@ -1,4 +1,4 @@
-const React = require('React');
+const React = require('react');
 //const ReactReconciler = require('react-dom/lib/ReactReconciler');
 const ReactFiberReconciler = require('react-dom/lib/ReactFiberReconciler');
 const expect = require('chai').expect;
