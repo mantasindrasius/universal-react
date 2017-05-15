2cc316d6f1689cd7cd16001c7d56540ea2114280
Mantas Indrašius <mantasi@wix.com>
2017-05-15 21:16:52 +0300

    Fiber reconciler

--- app/test/learning/react-noop.spec.js
+++ app/test/learning/react-noop.spec.js
@@ -1,5 +1,8 @@
 const React = require('React');
+//const ReactReconciler = require('react-dom/lib/ReactReconciler');
+const ReactFiberReconciler = require('react-dom/lib/ReactFiberReconciler');
 const expect = require('chai').expect;
+const emptyObject = require('fbjs/lib/emptyObject');
 
 describe('React', function() {
   it('handle click with dummy renderer', () => {
@@ -8,6 +11,12 @@ describe('React', function() {
     context.click();
 
     expect(context.getState().isToggleOn).to.be.true;
+  });
+
+  it('render with reconciler', async() => {
+    let rendered = await DummyReconcilerRenderer.render(ComponentUnderTest);
+    
+    expect(rendered.type).to.equal('a');
   });
 });
 
@@ -20,9 +29,12 @@ class ComponentUnderTest extends React.Component {
   }
 
   componentDidMount() {
+    console.log('Mount!');
   }
 
   render() {
+    console.log('Render!');
+
     return <div>
       <span prop="1">Hello 1</span>
       <span prop="2">Hello 2</span>
@@ -36,6 +48,190 @@ class ComponentUnderTest extends React.Component {
     }));
   }
 };
+
+var incompleteCallbacks = 0;
+
+function waitDone() {
+  return new Promise(completer);
+}
+
+function completer(fulfill) {
+  if (incompleteCallbacks == 0) {
+    fulfill();
+  } else {
+    process.nextTick(() => completer(fulfill));
+  }
+}
+
+const DummyReconcilerRenderer = new function() {
+  var rootContainer = null;
+  
+  const container = {
+    id: 'ze-container',
+    result: []
+  };
+
+  this.render = async (element) => {
+    //let element = new Component({});
+
+    rootContainer = DummyFiberReconciler.createContainer(container);
+    
+    DummyFiberReconciler.updateContainer(<a>Hi</a>, rootContainer);
+
+    await waitDone();
+    
+    return container.result[0];
+  }
+
+  this.unmountComponentAtNode = (container) => {
+    var root = rootContainer;
+    if (root) {
+      // TODO: Is it safe to reset this now or should I wait since this
+      // unmount could be deferred?
+      rootContainer = null;
+      DummyFiberReconciler.unmountContainer(root);
+    }
+  }
+}
+
+// const DummyReconcilerRenderer = {
+//   create: (element) => {
+//     return ReactTestMount.render(element, options);
+//   },
+//   /* eslint-disable camelcase */
+//   unstable_batchedUpdates: ReactUpdates.batchedUpdates,
+//   /* eslint-enable camelcase */
+// };
+
+const DummyFiberReconciler = ReactFiberReconciler({
+  getRootHostContext() {
+    console.log('getRootHostContext');
+    
+    return emptyObject;
+  },
+  updateContainer(container, children) {    
+    console.log('Update container', container, children);
+  },
+  // commitUpdate(instance, oldProps, newProps, children) {
+  // },
+  appendChild(parentInstance, child) {
+    console.log('appendChild', parentInstance, child);
+
+    parentInstance.result.push(child);
+  },
+  beginUpdate() {
+    console.log('beginUpdate');
+  },
+  appendInitialChild(parentInstance, child) {
+    console.log('appendInitialChild', parentInstance, child);
+
+    if (typeof child === 'string') {
+      // Noop for string children of Text (eg <Text>{'foo'}{'bar'}</Text>)
+      return;
+    }
+  },
+
+  commitTextUpdate(textInstance, oldText, newText) {
+    console.log('commitTextUpdate', textInstance, oldText, newText);
+    // Noop
+  },
+
+  commitUpdate(instance, type, oldProps, newProps) {
+    console.log('Commit update', instance, oldProps, newProps, children);
+
+    instance._applyProps(instance, newProps, oldProps);
+  },
+
+  createInstance(type, props, internalInstanceHandle) {
+    console.log('createInstance', type, props, internalInstanceHandle);
+
+    return {
+      type, props
+    };
+  },
+
+  createTextInstance(text, rootContainerInstance, internalInstanceHandle) {
+    console.log('createTextInstance', text, rootContainerInstance, internalInstanceHandle);
+    return text;
+  },
+
+  finalizeInitialChildren(domElement, type, props) {
+    console.log('finalizeInitialChildren', domElement, type, props);
+    // Noop
+  },
+
+  insertBefore(parentInstance, child, beforeChild) {
+    console.log('insertBefore', parentInstance, child, beforeChild);
+    invariant(
+      child !== beforeChild,
+      'ReactART: Can not insert node before itself'
+    );
+
+    child.injectBefore(beforeChild);
+  },
+
+  prepareForCommit() {
+    console.log('prepareForCommit');
+    // Noop
+  },
+
+  prepareUpdate(domElement, type, oldProps, newProps) {
+    console.log('prepareUpdate', domElement, type, oldProps, newProps);
+    return true;
+  },
+
+  removeChild(parentInstance, child) {
+    console.log('removeChild', parentInstance, child);
+
+    destroyEventListeners(child);
+
+    child.eject();
+  },
+
+  resetAfterCommit() {
+    console.log('resetAfterCommit');
+    // Noop
+  },
+
+  resetTextContent(domElement) {
+    console.log('resetTextContent', domElement);
+    // Noop
+  },
+
+  getChildHostContext() {
+    console.log('getChildHostContext');
+    return null;
+  },
+
+  scheduleDeferredCallback(callback) {
+    incompleteCallbacks++;
+
+    process.nextTick(() => {
+      try {
+        callback({
+          didTimeout: true,
+          timeRemaining() {
+            return 100;
+          }
+        });
+      } finally {
+        incompleteCallbacks--;
+        console.log('Work done, remaining', incompleteCallbacks);
+      }
+    });
+  },
+
+  shouldSetTextContent(props) {
+    console.log('shouldSetTextContent', props);
+
+    return (
+      typeof props.children === 'string' ||
+      typeof props.children === 'number'
+    );
+  },
+
+  useSyncScheduling: true,
+});
 
 const DummyRenderer = new function() {
   this.render = (Component) => {
